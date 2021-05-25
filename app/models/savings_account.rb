98e66896857ce241430a4a473a8aea31d851ccb0
Emiliano Menéndez <emystein@gmail.com>
2021-05-25 10:28:25 -0300

    Savings Account balance.

--- app/models/savings_account.rb
+++ app/models/savings_account.rb
@@ -1,4 +1,9 @@
 class SavingsAccount < ApplicationRecord
   belongs_to :user
   has_many :money_transactions
+
+  def balance
+    money_transactions.filter{ |t| t.direction == 'deposit' }.sum(&:amount) -
+      money_transactions.filter{ |t| t.direction == 'withdrawal' }.sum(&:amount)
+  end
 end
--- test/models/savings_account_test.rb
+++ test/models/savings_account_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
 class SavingsAccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'initial balance should be 0' do
+    account = SavingsAccount.new
+
+    assert account.balance == 0
+  end
+
+  test 'deposit 10 should increase balance by 10' do
+    account = SavingsAccount.new
+    account.money_transactions.new(direction: 'deposit', amount: 10)
+    account.save
+
+    assert account.balance == 10
+  end
+
+  test 'withdraw 10 should decrease balance by 10' do
+    account = SavingsAccount.new
+    account.money_transactions.new(direction: 'deposit', amount: 10)
+    account.money_transactions.new(direction: 'withdrawal', amount: 10)
+    account.save
+
+    assert account.balance == 0
+  end
 end
