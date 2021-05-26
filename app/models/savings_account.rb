e07698befc20740ddda5687ba3a643792af887a6
Emiliano Menéndez <emystein@gmail.com>
2021-05-26 00:00:02 -0300

    Don't accept negative amounts in transactions.

--- app/models/savings_account.rb
+++ app/models/savings_account.rb
@@ -7,12 +7,18 @@ class SavingsAccount < ApplicationRecord
   end
 
   def credit(amount)
+    raise RuntimeError unless amount.to_i.positive?
+
     money_transactions.new(amount: amount)
+
     save
   end
 
   def debit(amount)
+    raise RuntimeError unless amount.to_i.positive?
+
     money_transactions.new(amount: - amount.to_i)
+
     save
   end
 end
--- test/models/savings_account_test.rb
+++ test/models/savings_account_test.rb
@@ -4,7 +4,7 @@ class SavingsAccountTest < ActiveSupport::TestCase
   test 'initial balance should be 0' do
     account = SavingsAccount.new
 
-    assert account.balance == 0
+    assert account.balance.zero?
   end
 
   test 'credit 10 should increase balance by 10' do
@@ -12,6 +12,14 @@ class SavingsAccountTest < ActiveSupport::TestCase
     account.credit(10)
 
     assert account.balance == 10
+  end
+
+  test 'credit negative amount should throw an error' do
+    account = SavingsAccount.new
+
+    assert_raises RuntimeError do
+      account.credit(-10)
+    end
   end
 
   test 'debit 10 should decrease balance by 10' do
@@ -26,6 +34,14 @@ class SavingsAccountTest < ActiveSupport::TestCase
     account.credit(10)
     account.debit(10)
 
-    assert account.balance == 0
+    assert account.balance.zero?
+  end
+
+  test 'debit negative amount should throw an error' do
+    account = SavingsAccount.new
+
+    assert_raises RuntimeError do
+      account.debit(-10)
+    end
   end
 end
