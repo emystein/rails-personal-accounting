252eeb318f640cb7400ed4332d22dab6b17ebd03
Emiliano Menéndez <emystein@gmail.com>
2021-06-05 21:04:06 -0300

    Define operations in UsersController to show as buttons.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,18 @@ class UsersController < ApplicationController
   def show
     @savings_accounts = current_user.savings_accounts
     @currencies = current_user.account_currencies
+    @operations = {
+      deposit: 'Deposit',
+      withdrawal: 'Withdraw',
+      currency_sale: 'Sell'
+    }
   end
 
   def new_operation_on_account
     currency = params[:operate_with][:currency]
     account = current_user.account_for_currency(currency)
 
-    case params[:transaction]
+    case params[:operation]
     when 'deposit'
       redirect_to new_deposit_savings_account_url(account)
     when 'withdrawal'
