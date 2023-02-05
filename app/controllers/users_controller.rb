bc7f22c97e4b57e21bfe7e4cee72a60104ad0152
Emiliano Menéndez <emystein@gmail.com>
2023-02-05 16:38:51 -0300

    Add operations to each row of Saving Account list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,6 @@ class UsersController < ApplicationController
   def show
     @savings_accounts = current_user.savings_accounts
     @currencies = current_user.currencies
-    @operations = {
-      new_deposit: 'Deposit',
-      new_withdrawal: 'Withdraw',
-      new_currency_sale: 'Sell'
-    }
-  end
-
-  def new_operation_on_account
-    currency = params[:operate_with][:currency]
-    account = current_user.account_in(currency)
-    redirect_to "/savings_accounts/#{account.id}/#{params[:operation]}"
   end
 
   def create_savings_account
